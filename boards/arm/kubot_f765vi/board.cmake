61b8d2a4cd5a91ad093314104c9038fc657ae5b6
Krzysztof <k.pochwala@gmail.com>
2022-07-01 14:36:48 +0200

    support for black magic probe over wifi

--- boards/arm/kubot_f765vi/board.cmake
+++ boards/arm/kubot_f765vi/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=STM32F765VI" "--speed=4000")
+set(BOARD_FLASH_RUNNER blackmagicprobe)
+board_runner_args(blackmagicprobe "--gdb-serial=192.168.4.1:2022" "--monitor-frequency=1000000" "--connect-srst")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
